04398f5485ae847116a0322b68c36f165f6985a4
diptags <rd.pradipta@gmail.com>
2020-12-16 15:52:11 +0700

    Add Code to do Query

---
+++ main/utils/constants.py
@@ -0,0 +1,11 @@
+FUSEKI_URL = 'http://localhost:3030/ds/query'
+PREFIXES = \
+'''
+PREFIX snr: <http://skulite.org/snr/>
+PREFIX snp: <http://skulite.org/snp/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX dbr: <http://dbpedia.org/resource/>
+'''
--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from .utils.helpers import get_film_data
 
 def home(request):
     #todo ambil semua id dan nama dari film @reyhan
 
     return render(request, 'main/home.html')
-
 
 '''
 Fungsi untuk mengambil data spesifik film
@@ -30,10 +30,38 @@ def film_detail(request):
     context = {"detail_film" : detail_film} 
     return render(request, 'main/film_detail.html', context)
 
+'''
+Fungsi untuk melakukan pencarian film berdasarkan judul
+'''
+def film_search(request):
+    
+    #TODO ambil data json dari judul film yang dicari
+    #contoh query = http://127.0.0.1:8000/search?title=murder%20in%20%20park
+    
+    title_film = request.GET.get('title').lower()
+    title_formatted = '\"{}\"'.format(title_film)
+    query = \
+    '''
+    SELECT * WHERE { 
+        ?uri rdf:type snr:Film ;
+        snp:id ?id ;
+        snp:title ?title ;
+        FILTER (
+            contains(lcase(str(?title)), %s) 
+        )
+    }
+    ''' % (title_formatted)
 
+    get_film_data(query)
 
+    list_film = [
+        {'uri': {'type': 'uri', 'value': 'http://skulite.org/snr/A_Murder_in_the_Park'}, 'id': {'type': 'literal', 'value': '80044562'}, 'title': {'type': 'literal', 'value': 'A Murder in the Park'}},
+        {'uri': {'type': 'uri', 'value': 'http://skulite.org/snr/Jailbreak'}, 'id': {'type': 'literal', 'value': '80990658'}, 'title': {'type': 'literal', 'value': 'Jailbreak'}},
+        {'uri': {'type': 'uri', 'value': 'http://skulite.org/snr/Cristina'}, 'id': {'type': 'literal', 'value': '80076160'}, 'title': {'type': 'literal', 'value': 'Cristina'}}]
 
+    for film in list_film:
+        for key in film:
+            film[key] = film[key]['value']
 
-
-
-
+    context = {"list_film" : list_film} 
+    return render(request, 'main/list_film.html', context)
