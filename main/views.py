7b9761f42ce418e0ca8d3b1127c6aafc354a1407
Aziz Hudaya <azizhudaya39@gmail.com>
2020-12-16 15:17:55 +0700

    done home page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,55 @@
 from django.shortcuts import render
+import json
+import requests
+
+PREFIXES = \
+'''
+PREFIX snr: <http://skulite.org/snr/>
+PREFIX snp: <http://skulite.org/snp/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX dbr: <http://dbpedia.org/resource/>
+'''
+
+local_server_url = 'http://localhost:3030/ds/query'
+
+def get_response_from_local(query_to_send):
+    try:
+        response = requests.post(local_server_url, data={'query': query_to_send})
+        return response.json()['results']['bindings']
+    except ValueError:
+        print("JSON Bermasalah atau Jena Fuseki Return 400")
+        return None
+    except:
+        print("Jena Fuseki belum dijalankan")
+        return None
+
+
 
 def home(request):
-    #todo ambil semua id dan nama dari film @reyhan
+    movies_title = []
 
-    return render(request, 'main/home.html')
+    #query untuk mengambil semua id dan title pada ttl
+    query = \
+        '''
+        SELECT ?id ?title
+        WHERE {
+            {?movies rdf:type snr:Film.}
+            UNION
+            {?movies rdf:type snr:Series.}
+            ?movies snp:id ?id.
+            ?uri snp:id ?id.
+            ?uri snp:title ?title.
+
+        }
+        '''
+    local_query_result = get_response_from_local(PREFIXES + query)
+    for data in local_query_result:
+        movies_title.append({'id':data['id']['value'],'title':data['title']['value']})
+
+    return render(request, 'main/home.html',{'data_title':movies_title})
 
 
 '''
@@ -27,7 +73,7 @@ def film_detail(request):
     'duration': '91 min'
     }
 
-    context = {"detail_film" : detail_film} 
+    context = {"detail_film" : detail_film}
     return render(request, 'main/film_detail.html', context)
 
 
